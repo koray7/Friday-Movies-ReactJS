9bf42206c8fdd1409c701920e44730173dfbfa2e
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-04-15 17:26:13 -0500

    Dropdown settings

--- src/component/navbar/navbar.jsx
+++ src/component/navbar/navbar.jsx
@@ -18,7 +18,36 @@ const Navbar = () => {
                     <span>My List</span>
                 </div>
                 
-                <div className="right"></div>
+                <div className="right">
+                    <img 
+                        className="logo"
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu6GUuqAUrdzpX9P5kgk0OQ4lpMNACqyqSZla13QZyz5-VT6kClv_s45tNAjW8n3k3ykQ&usqp=CAU"
+                        alt="Search-icon"
+                    />
+
+                    <span>KID</span>
+                    <img 
+                        className="logo"
+                        src="https://cdn-icons-png.flaticon.com/512/1592/1592461.png"
+                        alt="Notification-icon"
+                    />
+                    <img 
+                        className="logo"
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/768px-User_icon_2.svg.png"
+                        alt="user-icon"
+                    />
+                    <div className="profile">
+                        <img 
+                            className="logo"
+                            src="https://icon-library.com/images/red_arrow_down-512.png"
+                            alt="arrowdown-icon"
+                        />
+                        <div className="options">
+                            <span>Settings</span>
+                            <span>Login</span>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>

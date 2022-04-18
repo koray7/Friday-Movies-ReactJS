0bd159d5b366331b9e4765009a19d29cfe3979b6
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-04-18 17:16:10 -0500

    Movie section

--- src/component/list/list.jsx
+++ src/component/list/list.jsx
@@ -1,7 +1,18 @@
-import './list.css'
-import ListItem from '../listItem/listItem'
+import './list.css';
+import ListItem from '../listItem/listItem';
+import { useRef } from 'react'
 
-const list = () => {
+const List = () => {
+
+    const listRef = useRef()
+
+    const handleClick = (direction) => {
+        let distance = listRef.current.getBoundingClientRect().x - 50
+        if(direction === "left") {
+            listRef.current.style.transform = `translateX(${230 + distance}px)`
+        }
+    }
+
     return (
         <div className="list">
             <span className="listTitle">Continue to watch</span>
@@ -10,6 +21,7 @@ const list = () => {
                     className="sliderArrow left"
                     src="https://c8.alamy.com/zooms/9/ff1d005be9514206b7c744b74c7b4b58/2ap32wa.jpg"
                     alt="arrowR"
+                    onClick={() => handleClick("left")}
                 />
 
                 <div className="container">
@@ -28,10 +40,11 @@ const list = () => {
                     className="sliderArrow right"
                     src="https://www.freeiconspng.com/thumbs/arrow-icon/big-left-arrow-icon-007358--icons-etc-15.png"
                     alt="arrowL"
+                    onclick={() => handleClick("right")}
                 />
             </div>
         </div>
     )
 }
 
-export default list;
+export default List;

b221f87645f8da3249536d51acfb2e624501b0fb
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-04-18 23:12:50 -0500

    Movies part

--- src/component/list/list.jsx
+++ src/component/list/list.jsx
@@ -11,14 +11,14 @@ const List = () => {
 
     const handleClick = (direction) => {
         setIsMoved(true)
-        let distance = listRef.current.getBoundingClientRect().x - 50
+        let distance = listRef.current.getBoundingClientRect().x
         if(direction === "left" && slideNumber > 0) {
             setSlideNumber(slideNumber - 1);
-            listRef.current.style.transform = `translateX(${207 + distance}px)`
+            listRef.current.style.transform = `translateX(${150 + distance}px)`
         }
         if(direction === "right" && slideNumber < 5) {
             setSlideNumber(slideNumber + 1)
-            listRef.current.style.transform = `translateX(${-253 + distance}px)`
+            listRef.current.style.transform = `translateX(${-300 + distance}px)`
         }
     }
 
--- src/component/listItem/listItem.jsx
+++ src/component/listItem/listItem.jsx
@@ -3,7 +3,7 @@ import './listItem.css'
 const ListItem = () => {
     return (
         <div className="listItem">
-        listItem
+        <img className="movies-img" src="https://assets-prd.ignimgs.com/2022/01/14/gameofthrones-allseasons-sq-1642120207458.jpg?width=300" alt="movie-img"/>
         </div>
     )
 }

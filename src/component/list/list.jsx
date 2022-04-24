42d10a85ccc155b0df866834d00ce5a2b1d18058
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-04-23 22:00:27 -0500

    Movie Table

--- src/component/list/list.jsx
+++ src/component/list/list.jsx
@@ -11,14 +11,14 @@ const List = () => {
 
     const handleClick = (direction) => {
         setIsMoved(true)
-        let distance = listRef.current.getBoundingClientRect().x
+        let distance = listRef.current.getBoundingClientRect().x - 50
         if(direction === "left" && slideNumber > 0) {
             setSlideNumber(slideNumber - 1);
-            listRef.current.style.transform = `translateX(${150 + distance}px)`
+            listRef.current.style.transform = `translateX(${305 + distance}px)`
         }
         if(direction === "right" && slideNumber < 5) {
             setSlideNumber(slideNumber + 1)
-            listRef.current.style.transform = `translateX(${-300 + distance}px)`
+            listRef.current.style.transform = `translateX(${-285 + distance}px)`
         }
     }
 

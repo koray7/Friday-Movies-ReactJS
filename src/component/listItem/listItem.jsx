7b52f32a93215e19efe0b2494593b45df69295d9
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-04-24 18:48:59 -0500

    Image fixed

--- src/component/listItem/listItem.jsx
+++ src/component/listItem/listItem.jsx
@@ -3,13 +3,15 @@ import './listItem.css'
 const ListItem = () => {
     return (
         <div className="listItem">
-            <img className="movies-img" src="https://assets-prd.ignimgs.com/2022/01/14/gameofthrones-allseasons-sq-1642120207458.jpg?width=300" alt="movie-img"/>
+            <div className="movies">
+                <img className="movies-img" src="https://assets-prd.ignimgs.com/2022/01/14/gameofthrones-allseasons-sq-1642120207458.jpg?width=300" alt="movie-img"/>
+            </div>
             <div className="itemInfo">
                 <div className="icons">
-                    <img className="movies-icon" src="https://cdn-icons-png.flaticon.com/512/1362/1362985.png" alt="play-icon"/>
-                    <img className="movies-icon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlLixtfbLjAS0QmRmcjY9d6o8xwGzRGEhhjTH1tN5gIcUTJUSTIDcRJPALLOeAGrRdnkM&usqp=CAU" alt="add-icon"/>
-                    <img className="movies-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Facebook_Thumb_icon.svg/640px-Facebook_Thumb_icon.svg.png" alt="play-icon"/>
-                    <img className="movies-icon" src="https://media.pocketnow.com/styles/large/public/2019/02/YouTube-dislike-button.jpg?itok=dbcQU5bm" alt="play-icon"/>
+                    <img id="movies-icon" src="https://cdn-icons-png.flaticon.com/512/1362/1362985.png" alt="play-icon"/>
+                    <img id="movies-icon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlLixtfbLjAS0QmRmcjY9d6o8xwGzRGEhhjTH1tN5gIcUTJUSTIDcRJPALLOeAGrRdnkM&usqp=CAU" alt="add-icon"/>
+                    <img id="movies-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Facebook_Thumb_icon.svg/640px-Facebook_Thumb_icon.svg.png" alt="play-icon"/>
+                    <img id="movies-icon" src="https://media.pocketnow.com/styles/large/public/2019/02/YouTube-dislike-button.jpg?itok=dbcQU5bm" alt="play-icon"/>
                     <button>Button</button>
                 </div>
             </div>

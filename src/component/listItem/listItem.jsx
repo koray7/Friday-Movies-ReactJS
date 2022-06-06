52a02a3ad66223de06465ee34ff616e91db779cb
Koray Kurtoglu <coreykurtoglu@gmail.com>
2022-06-06 12:33:41 -0500

    added list of movies

--- src/component/listItem/listItem.jsx
+++ src/component/listItem/listItem.jsx
@@ -4,21 +4,21 @@ import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 
 const ListItem = () => {
-    // return (
-    //     <div className="listItem">
-    //         <div className="movies">
-    //             <img className="movies-img" src="https://assets-prd.ignimgs.com/2022/01/14/gameofthrones-allseasons-sq-1642120207458.jpg?width=300" alt="movie-img"/>
-    //         </div>
-    //         <div className="itemInfo">
-    //             <div className="icons">
-    //                 <PlayArrowIcon style={{ fontSize: 38 }}/>
+    return (
+        <div className="listItem">
+            <div className="movies">
+                <img className="movies-img" src="https://assets-prd.ignimgs.com/2022/01/14/gameofthrones-allseasons-sq-1642120207458.jpg?width=300" alt="movie-img"/>
+            </div>
+            <div className="itemInfo">
+                <div className="icons">
+                    <PlayArrowIcon style={{ fontSize: 38 }}/>
                     
-    //                 <AddCircleIcon style={{ fontSize: 38 }}/>
+                    <AddCircleIcon style={{ fontSize: 38 }}/>
                     
-    //                 <button>Button</button>
-    //             </div>
-    //         </div>
-    //     </div>
+                    <button>Button</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
